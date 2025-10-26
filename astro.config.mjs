f902bea1cde54ec6faf407e8bccb915b6c95e503
JadenJSJ <jadenjsj@proton.me>
2025-10-26 22:31:41 +0800

    Github pages org?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bit-aics-hello-world-25.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
